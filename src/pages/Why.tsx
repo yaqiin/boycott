3f5d8fddbfd9a8b974c6ebe25b7a082a8cc25805
HouariZegai <houarizegai14@gmail.com>
2025-04-11 22:38:13 +0100

    refactor: improved the why boycott data

--- src/pages/Why.tsx
+++ src/pages/Why.tsx
@@ -1,47 +1,31 @@
-
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import { useLanguage } from "@/contexts/LanguageContext";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
-import {ArrowLeft, ArrowRight, BicepsFlexed, Braces, CircleHelp, Crosshair, HeartHandshake} from "lucide-react";
+import {useLanguage} from "@/contexts/LanguageContext";
+import {Button} from "@/components/ui/button";
+import {Link} from "react-router-dom";
+import * as LucideIcons from "lucide-react";
+import {ArrowLeft, ArrowRight, LucideProps} from "lucide-react";
+import whyBoycottData from '@/../data/why_boycott.json';
+import * as react from "react";
+import {ReactNode} from "react";
 
 const Why = () => {
-  const { t, isRTL } = useLanguage();
+  const {t, isRTL, language} = useLanguage();
 
-  const whyList = [
-    {
-      icon: CircleHelp,
-      subtitle: t("whyBoycottSubtitle1"),
-      paragraph: t("whyBoycottParagraph1"),
-    },
-    {
-      icon: BicepsFlexed,
-      subtitle: t("whyBoycottSubtitle2"),
-      paragraph: t("whyBoycottParagraph2"),
-    },
-    {
-      icon: Crosshair,
-      subtitle: t("whyBoycottSubtitle3"),
-      paragraph: t("whyBoycottParagraph3"),
-    },
-    {
-      icon: HeartHandshake,
-      subtitle: t("whyBoycottSubtitle4"),
-      paragraph: t("whyBoycottParagraph4"),
-    },
-  ];
+  function getIcon(iconName: string) {
+    return LucideIcons[iconName as keyof typeof LucideIcons];
+  }
 
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
+      <Header/>
       <main className="flex-grow yaqiin-container py-8 animate-fade-in">
         <Link to="/">
           <Button variant="ghost" className="mb-6 flex items-center gap-2">
             {isRTL ?
-              <ArrowRight size={16} />
+              <ArrowRight size={16}/>
               :
-              <ArrowLeft size={16} />
+              <ArrowLeft size={16}/>
             }
             <span>{t("backToProducts")}</span>
           </Button>
@@ -53,21 +37,27 @@ const Why = () => {
           </h1>
           <section className="prose p-6 dark:prose-invert prose-yaqiin">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {whyList.map((item, index) => (
-                <div key={index} className="p-4 border rounded-lg hover:shadow-md">
-                  <div className="flex items-center gap-2 mb-2">
-                    <item.icon className="text-yaqiin-800 dark:text-yaqiin-500 h-5 w-5"/>
-                    <h3 className="font-semibold text-yaqiin-800 dark:text-yaqiin-500">{item.subtitle}</h3>
+              {whyBoycottData.map((item, index) => {
+                const IconComponent = getIcon(item.icon);
+                return (
+                  <div key={index} className="p-4 border rounded-lg hover:shadow-md">
+                    <div className="flex items-center gap-2 mb-2">
+                      {IconComponent && (
+                        <IconComponent className="text-yaqiin-800 dark:text-yaqiin-500 h-5 w-5"/>
+                      )}
+                      <h3 className="font-semibold text-yaqiin-800 dark:text-yaqiin-500">
+                        {item.title[language]}
+                      </h3>
+                    </div>
+                    <p className="text-mosque-accent">{item.description[language]}</p>
                   </div>
-                  <p className="text-mosque-accent">{item.paragraph}</p>
-                </div>
-              ))
-              }
+                );
+              })}
             </div>
           </section>
         </div>
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
